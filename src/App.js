b71ae45da6959d53791df5f08ef9ae3bc229c91c
hklim00 <g5bugi82@gmail.com>
2022-08-10 10:26:16 +0900

    컴포넌트 파일들 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+
+import Content from './components/main/Content';
+import Visual from './components/main/Visual';
+
+import Community from './components/sub/Community';
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+import Location from './components/sub/Location';
+import Members from './components/sub/Members';
+import Youtube from './components/sub/Youtube';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+			<Footer />
+
+			<Content />
+			<Visual />
+
+			<Community />
+			<Department />
+			<Gallery />
+			<Location />
+			<Members />
+			<Youtube />
+		</>
 	);
 }
 
