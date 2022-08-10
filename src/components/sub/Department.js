ecf3d0d3f279124c0a97a2b38a4c853c4ec50728
hklim00 <g5bugi82@gmail.com>
2022-08-11 03:10:04 +0900

    department 데 가져오기

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,7 +1,36 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Department() {
-	return <Layout name={'Department'}>Department</Layout>;
+	const [Members, setMembers] = useState([]);
+	const path = process.env.PUBLIC_URL;
+
+	useEffect(() => {
+		axios.get(`${path}/DB/members.json`).then((json) => {
+			setMembers(json.data.members);
+		});
+	}, []);
+
+	return (
+		<Layout name={'Department'}>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='inner'>
+							<div className='pic'>
+								<img src={`${path}/img/${member.pic}`} alt='' />
+							</div>
+							<div className='txt'>
+								<h2>{member.name}</h2>
+								<p>{member.position}</p>
+							</div>
+						</div>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Department;
