44bba3214f8b9528f889cbfa71f8e73d2831c60d
hklim00 <g5bugi82@gmail.com>
2022-08-10 10:43:44 +0900

    기본 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route } from 'react-router-dom';
+
+// common
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 
+// main
 import Content from './components/main/Content';
 import Visual from './components/main/Visual';
 
+// sub
 import Community from './components/sub/Community';
 import Department from './components/sub/Department';
 import Gallery from './components/sub/Gallery';
@@ -15,17 +20,19 @@ function App() {
 	return (
 		<>
 			<Header />
+			<Route exact path='/'>
+				<Content />
+				<Visual />
+			</Route>
+
+			<Route path='/department' component={Department} />
+			<Route path='/community' component={Community} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/location' component={Location} />
+			<Route path='/members' component={Members} />
+
 			<Footer />
-
-			<Content />
-			<Visual />
-
-			<Community />
-			<Department />
-			<Gallery />
-			<Location />
-			<Members />
-			<Youtube />
 		</>
 	);
 }
