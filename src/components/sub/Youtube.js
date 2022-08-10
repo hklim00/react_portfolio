cafabdb807e4cf8e8c5d71f4a72f3e57be2439c5
hklim00 <g5bugi82@gmail.com>
2022-08-11 03:23:08 +0900

    youtube 데이터 볼러오기

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,7 +1,40 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Youtube() {
-	return <Layout name={'Youtube'}>Youtube</Layout>;
+	const [Videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const key = 'AIzaSyBDL1S8asY8CR73ihG02orQB3BdWha5F1A';
+		const playlistId = 'PL_RxE5V-zXWLz8bPJ5xi6dsdqg2mnwgPr';
+		const num = 6;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
+
+		axios.get(url).then((json) => {
+			setVideos(json.data.items);
+		});
+	}, []);
+
+	return (
+		<Layout name={'Youtube'}>
+			{Videos.map((vid) => {
+				return (
+					<article key={vid.id}>
+						<div className='inner'>
+							<div className='pic'>
+								<img src={vid.snippet.thumbnails.high.url} alt='' />
+							</div>
+							<div className='txt'>
+								<h2>{vid.snippet.title}</h2>
+								<p>{vid.snippet.description}</p>
+							</div>
+						</div>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Youtube;
