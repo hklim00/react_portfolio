7174a4c155214e1b0b87b5fa832f0f275ca04ca4
hklim00 <g5bugi82@gmail.com>
2022-08-11 03:34:10 +0900

    카카오 지도 가져오기

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,7 +1,23 @@
 import Layout from '../common/Layout';
+import { useState, useEffect, useRef } from 'react';
 
 function Location() {
-	return <Layout name={'Location'}>Location</Layout>;
+	const { kakao } = window;
+	const container = useRef(null);
+	const option = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		const map = new kakao.maps.Map(container.current, option);
+	}, []);
+
+	return (
+		<Layout name={'Location'}>
+			<div id='map' ref={container}></div>
+		</Layout>
+	);
 }
 
 export default Location;
