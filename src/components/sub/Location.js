3145f34a976712c2fa3a24d3ffc978afda130b5d
hklim00 <g5bugi82@gmail.com>
2022-08-16 03:51:16 +0900

    location 상단  레이아웃 완료

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,5 +1,5 @@
 import Layout from '../common/Layout';
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 function Location() {
 	const { kakao } = window;
@@ -22,7 +22,32 @@ function Location() {
 
 	return (
 		<Layout name={'Location'}>
-			<div id='map' ref={container}></div>
+			<div className='wrap'>
+				<div className='map'>
+					<div id='map' ref={container}></div>
+				</div>
+
+				<div className='box'>
+					<dl>
+						<dt>PHONE</dt>
+						<dd>+0 124 564 33-22</dd>
+						<dd>+0 124 564 33-22</dd>
+						<dd>+0 124 564 33-22</dd>
+					</dl>
+					<dl>
+						<dt>EMAIL</dt>
+						<dd>archtech@email.com</dd>
+					</dl>
+					<dl>
+						<dt>ADDRESS</dt>
+						<dd>
+							KOREA SEOUL.
+							<br />
+							453 Gangnam street BD 33
+						</dd>
+					</dl>
+				</div>
+			</div>
 		</Layout>
 	);
 }
